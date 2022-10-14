56894bb9a100fee8f3b00e0936e28df8ea54d165
Cesar Arcos-Gonzalez <60860385+CAG9@users.noreply.github.com>
2022-10-13 22:59:57 -0500

    Create SampleContractInjectedMetamask.sol

---
+++ SampleContractInjectedMetamask.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+contract SampleContract {
+
+    string public myString = "Hello World";
+    
+    
+    function updateString(string memory _newString) public {
+        myString = _myString;
+    }
+
+}
